8a941fbe06dbbb53944812be031f5b41ffdd1887
Yu Tian <yu.tian@aalto.fi>
2024-09-11 17:56:38 +0300

    parallelize image extraction

--- extract_images.sh
+++ extract_images.sh
@@ -2,10 +2,12 @@
 mkdir -p /tmp/ILSVRC2012_extracted
 
 tar -xvf /scratch/shareddata/dldata/imagenet/ILSVRC2012_img_train.tar -C /tmp/ILSVRC2012_extracted 
-for tarfile in /tmp/ILSVRC2012_extracted/*.tar; do
-    foldername=$(basename "$tarfile" .tar)
-    mkdir -p /tmp/ILSVRC2012_extracted/"$foldername"
-    tar -xf "$tarfile" -C /tmp/ILSVRC2012_extracted/"$foldername"
-done
 
+# Parallel extraction of tar files
+find /tmp/ILSVRC2012_extracted -name "*.tar" | parallel -j+0 '
+    foldername=$(basename {} .tar)
+    mkdir -p /tmp/ILSVRC2012_extracted/"$foldername"
+    tar -xf {} -C /tmp/ILSVRC2012_extracted/"$foldername"
+    rm {}
+'
 
